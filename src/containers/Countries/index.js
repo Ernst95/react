012221f444b1c2bfefc8634e858d750f8b037097
Ernst Chapman <ErnstC@AWARDS.CO.ZA>
2018-09-04 15:23:29 +0200

    added error handling

--- src/containers/Countries/index.js
+++ src/containers/Countries/index.js
@@ -4,7 +4,10 @@ import CountriesList from '../../components/CountriesList';
 class Countries extends Component {
 
     state = {
-        countries: []
+        countries: [],
+        countriesName: '',
+        isFetching: false, 
+        error: null
     }
 
    /* componentDidMount() {
@@ -27,19 +30,47 @@ class Countries extends Component {
     }*/
 
     onCountriesInputChange = e => {
+
+        this.setState({ countriesName: e.target.value, isFetching: true });
+
         fetch(`https://restcountries.eu/rest/v2/name/${e.target.value}`)
-            .then(response => response.json())
-            .then(json => this.setState({ countries: json }))
+            .then(response => {
+                if(response.ok) {
+                    return response.json();
+                }
+                else {
+                    throw new Error('Something went wrong ...');
+                }
+            })
+            .then(json => this.setState({ countries: json, isFetching: false }))
+            .catch(error => this.setState({ error, countries: [], isFetching: false }));
     }
 
     render() {
+
+        const { countries, countriesName, isFetching, error } = this.state;
+
         return (
             <div>
-                The length of countries array : {this.state.countries.length}
                 <div> 
-                    <input type = "text" onChange = {this.onCountriesInputChange} />
+                    <input value = {countriesName} type = "text" onChange = {this.onCountriesInputChange} />
                 </div>
-                <CountriesList list = {this.state.countries}/>
+                {
+                    !isFetching && countries.length === 0 && countriesName.trim() === '' &&
+                    <p> Please enter countries name into the input </p>
+                }
+                {
+                    !isFetching && countries.length === 0 && countriesName.trim() !== '' && error &&
+                    <p> No countries have been found with this name</p>
+                }
+                {
+                    isFetching && 
+                    <p> Loading...</p>
+                }
+                {
+                    !isFetching &&
+                    <CountriesList list = {this.state.countries}/>
+                }
             </div>
         )
     }
